e4b27a05c88e49937bc101f8b91ecd46de2db84c
Santosh <dhitalsantoshgsd999@gmail.com>
2025-12-09 12:33:25 +0545

    update function for endpoints

--- app/crud.py
+++ app/crud.py
@@ -1,17 +1,16 @@
-from fastapi import Depends
 from app.database.db_session import get_db
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session
 from app.schemas.notes import NoteCreate, NoteOut, NoteUpdate
 from app.models.note import Note
-from app.core.security import get_current_user
+from app.models.user import User
 
 
-def get_note(note_id: int, db:session = Depends(get_db), user = Depends(get_current_user)):
+def get_note(note_id: int, db: Session):
     return (
         db.query(Note).filter(Note.id == note_id).first()
     )
 
-def create_note(note: NoteCreate, db:session = Depends(get_db), user = Depends(get_current_user)):
+def create_note(note: NoteCreate, db: Session, user: User):
     new_note = Note(
         title = note.title,
         content = note.content,
@@ -23,17 +22,19 @@ def create_note(note: NoteCreate, db:session = Depends(get_db), user = Depends(g
     return new_note
 
 
-def update_note(note_id: int, note = NoteUpdate, db:session = Depends(get_db), user = Depends(get_current_user)):
+def update_note(note_id: int, note: NoteUpdate, db: Session, user: User):
     db_note = db.query(Note).filter(Note.id == note_id, Note.owner_id == user.id).first()
     if db_note:
-        db_note.title = note.title
-        db_note.content = note.content
+        if note.title is not None:
+            db_note.title = note.title
+        if note.content is not None:
+            db_note.content = note.content
         db.commit()
         db.refresh(db_note)
 
     return db_note
 
-def delete_note(note_id: int, db:session = Depends(get_db)):
+def delete_note(note_id: int, db: Session):
     db_note = db.query(Note).filter(Note.id == note_id).first()
     if db_note:
         db.delete(db_note)
