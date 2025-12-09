7628a9dc86ed923274eed2a499029e1d19a429ff
Santosh <dhitalsantoshgsd999@gmail.com>
2025-12-09 12:31:35 +0545

    update models settings

--- app/models/note.py
+++ app/models/note.py
@@ -6,7 +6,7 @@ class Note(Base):
     __tablename__ = "notes"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
+    title = Column(String(100), nullable=False)
     content = Column(Text, nullable=False)
     summary = Column(Text, nullable=True)
     owner_id = Column(Integer, ForeignKey("users.id"))
